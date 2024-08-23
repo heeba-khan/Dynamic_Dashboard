6a8ed62ac8e1160378dd287310d3b4a9815e5f37
heeba-khan <heebak45@gmail.com>
2024-08-24 00:00:01 +0530

    Modified search functionality

--- dynamic-dashboard/src/App.jsx
+++ dynamic-dashboard/src/App.jsx
@@ -9,7 +9,6 @@ const App = () => {
   return (
     <Provider store={store}>
       <div className="App">
-       
         <Dashboard />
       </div>
     </Provider>
--- dynamic-dashboard/src/components/Dashboard.jsx
+++ dynamic-dashboard/src/components/Dashboard.jsx
@@ -2,23 +2,32 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Category from './category/Category';
-import AddWidgetForm from './category/AddWidget';
-import Widget from './category/Widget';
 import SearchBar from './SearchBar';
 
 const Dashboard = () => {
   const categories = useSelector((state) => state.categories);
   const searchTerm=useSelector((state)=>state.searchTerm.toLowerCase())
 
-  const filterCategories=categories.filter((category)=>
-    category.name.toLowerCase().includes(searchTerm)
-)
+//   const filterCategories=categories.filter((category)=>
+//     category.name.toLowerCase().includes(searchTerm)
+// )
+
+const filterCategories = categories.map((category) => {
+  const filteredWidgets = category.widgets.filter((widget) =>
+    widget.name.toLowerCase().includes(searchTerm)
+  );
+
+  return {
+    ...category,
+    widgets: filteredWidgets,
+  };
+});
 
   return (
     <div className='dashboard'>
         <div className="header">
             <h1 className='dash-heading'>Dynamic Dashboard</h1>
-            <SearchBar  />
+            <SearchBar />
         </div>
       {filterCategories.map((category) => (
         <Category key={category.id} category={category} />
